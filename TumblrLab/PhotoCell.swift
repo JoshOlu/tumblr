48c9d455ea10e4a0f05fdca9ff7a4058ceba0cbb
Josh Olumese <joshlume@fb.com>
2017-06-21 14:40:06 -0700

    first commit

---
+++ TumblrLab/PhotoCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoCell.swift
+//  TumblrLab
+//
+//  Created by Josh Olumese on 6/21/17.
+//  Copyright © 2017 Josh Olumese. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
